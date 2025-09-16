0d92a5e396b68f904d035e78a417c51629d8402a
Lau163 <anncervan11@gmail.com>
2025-09-16 12:17:48 -0600

    add view
    Se añadio a visual studio el codigo de la vista de pacientes

---
+++ views/paciente/index.view.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+    <div class="w-[1440px] h-[900px] relative bg-white overflow-hidden">
+  <div class="w-[1440px] h-[900px] left-0 top-0 absolute bg-slate-300"></div>
+  <div class="w-[1086px] h-96 left-[354px] top-0 absolute bg-emerald-500"></div>
+  <div class="w-96 h-[900px] left-0 top-[-2px] absolute bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+  <div class="w-72 h-20 left-[38px] top-[39px] absolute bg-stone-300"></div>
+  <img class="w-6 h-6 left-[107px] top-[337px] absolute" src="https://placehold.co/25x25" />
+  <img class="w-6 h-6 left-[107px] top-[388px] absolute" src="https://placehold.co/25x25" />
+  <img class="w-9 h-9 left-[102px] top-[439px] absolute" src="https://placehold.co/35x35" />
+  <img class="w-6 h-6 left-[107px] top-[505px] absolute" src="https://placehold.co/25x25" />
+  <div class="left-[149px] top-[283px] absolute justify-start text-black text-xl font-bold font-['Roboto']">Clientes</div>
+  <div class="left-[149px] top-[339px] absolute justify-start text-zinc-500 text-xl font-bold font-['Roboto']">Reservas</div>
+  <div class="left-[149px] top-[507px] absolute justify-start text-zinc-500 text-xl font-bold font-['Roboto']">Reportes</div>
+  <div class="left-[151px] top-[395px] absolute justify-start text-zinc-500 text-xl font-bold font-['Roboto']">Servicios</div>
+  <div class="left-[151px] top-[451px] absolute justify-start text-zinc-500 text-xl font-bold font-['Roboto']">Mascotas</div>
+  <div class="w-72 h-0 left-[27px] top-[584px] absolute shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-1 outline-offset-[-0.50px] outline-zinc-400"></div>
+  <img class="w-6 h-6 left-[107px] top-[281px] absolute" src="https://placehold.co/25x25" />
+  <div class="w-8 h-0 left-[69px] top-[278px] absolute origin-top-left rotate-90 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-4 outline-offset-[-2px] outline-neutral-500"></div>
+  <div class="w-[1024px] h-[569px] left-[385px] top-[229px] absolute bg-white rounded-[10px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+  <div class="w-80 h-9 left-[894px] top-[43px] absolute rounded-[20px] border border-white"></div>
+  <div class="w-24 h-6 left-[939px] top-[50px] absolute justify-start text-white text-base font-normal font-['Roboto']">Buscar</div>
+  <div class="w-24 h-6 left-[1320px] top-[50px] absolute justify-start text-white text-base font-normal font-['Roboto']">Usuario</div>
+  <div class="w-12 h-12 left-[1251px] top-[36px] absolute bg-stone-300 rounded-full"></div>
+  <div class="left-[393px] top-[47px] absolute justify-start text-white text-2xl font-bold font-['Roboto']">Clientes</div>
+  <div class="left-[411px] top-[253px] absolute justify-start text-black/70 text-2xl font-bold font-['Roboto']">Clientes</div>
+  <div class="w-[1024px] h-16 left-[385px] top-[295px] absolute bg-slate-300 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+  <div class="left-[588px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">CLIENTE</div>
+  <div class="left-[487px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">CÓDIGO</div>
+  <div class="left-[670px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">FECHA  REGISTRO</div>
+  <div class="left-[824px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">DIRECCIÓN</div>
+  <div class="left-[959px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">EMAIL</div>
+  <div class="left-[1066px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">TELÉFONO</div>
+  <div class="left-[1177px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">MONTO</div>
+  <div class="left-[1293px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">OPCIONES</div>
+  <div class="left-[432px] top-[321px] absolute justify-start text-neutral-500/90 text-base font-black font-['Roboto']">#</div>
+  <img class="w-6 h-6 left-[1305px] top-[391px] absolute" src="https://placehold.co/25x25" />
+  <img class="w-6 h-6 left-[1340px] top-[391px] absolute" src="https://placehold.co/25x25" />
+  <div class="left-[570px] top-[395px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Luis Gonzales</div>
+  <div class="left-[487px] top-[395px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">R0100</div>
+  <div class="left-[692px] top-[395px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">10-02-2020</div>
+  <div class="left-[821px] top-[395px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Km 40 Lurin</div>
+  <div class="left-[925px] top-[395px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Luis@gmail.com</div>
+  <div class="left-[1063px] top-[395px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">987686546</div>
+  <div class="left-[1190px] top-[395px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">400</div>
+  <div class="left-[432px] top-[395px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">1</div>
+  <div class="w-[1025px] h-0 left-[384px] top-[448.01px] absolute outline outline-1 outline-offset-[-0.50px] outline-zinc-400"></div>
+  <img class="w-6 h-6 left-[1305px] top-[481px] absolute" src="https://placehold.co/25x25" />
+  <img class="w-6 h-6 left-[1340px] top-[481px] absolute" src="https://placehold.co/25x25" />
+  <div class="left-[570px] top-[485px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Luis Gonzales</div>
+  <div class="left-[487px] top-[485px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">R0101</div>
+  <div class="left-[692px] top-[485px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">10-02-2020</div>
+  <div class="left-[821px] top-[485px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Km 40 Lurin</div>
+  <div class="left-[925px] top-[485px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Luis@gmail.com</div>
+  <div class="left-[1063px] top-[485px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">987686546</div>
+  <div class="left-[1190px] top-[485px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">400</div>
+  <div class="left-[432px] top-[485px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">2</div>
+  <div class="w-[1025px] h-0 left-[384px] top-[538.01px] absolute outline outline-1 outline-offset-[-0.50px] outline-zinc-400"></div>
+  <img class="w-6 h-6 left-[1305px] top-[571px] absolute" src="https://placehold.co/25x25" />
+  <img class="w-6 h-6 left-[1340px] top-[571px] absolute" src="https://placehold.co/25x25" />
+  <div class="left-[570px] top-[575px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Luis Gonzales</div>
+  <div class="left-[487px] top-[575px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">R0102</div>
+  <div class="left-[692px] top-[575px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">10-02-2020</div>
+  <div class="left-[821px] top-[575px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Km 40 Lurin</div>
+  <div class="left-[925px] top-[575px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Luis@gmail.com</div>
+  <div class="left-[1063px] top-[575px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">987686546</div>
+  <div class="left-[1190px] top-[575px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">400</div>
+  <div class="left-[432px] top-[575px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">3</div>
+  <div class="w-[1025px] h-0 left-[384px] top-[628.01px] absolute outline outline-1 outline-offset-[-0.50px] outline-zinc-400"></div>
+  <img class="w-6 h-6 left-[1305px] top-[661px] absolute" src="https://placehold.co/25x25" />
+  <img class="w-6 h-6 left-[1340px] top-[661px] absolute" src="https://placehold.co/25x25" />
+  <div class="left-[570px] top-[665px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Luis Gonzales</div>
+  <div class="left-[487px] top-[665px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">R0103</div>
+  <div class="left-[692px] top-[665px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">10-02-2020</div>
+  <div class="left-[821px] top-[665px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Km 40 Lurin</div>
+  <div class="left-[925px] top-[665px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">Luis@gmail.com</div>
+  <div class="left-[1063px] top-[665px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">987686546</div>
+  <div class="left-[1190px] top-[665px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">400</div>
+  <div class="left-[432px] top-[665px] absolute justify-start text-zinc-700 text-base font-medium font-['Roboto']">4</div>
+  <div class="w-[1025px] h-0 left-[384px] top-[718.01px] absolute outline outline-1 outline-offset-[-0.50px] outline-zinc-400"></div>
+  <img class="w-7 h-7 left-[1264px] top-[743px] absolute" src="https://placehold.co/30x30" />
+  <img class="w-7 h-7 left-[1194px] top-[743px] absolute" src="https://placehold.co/30x30" />
+  <img class="w-7 h-7 left-[1334px] top-[743px] absolute" src="https://placehold.co/30x30" />
+  <img class="w-7 h-7 left-[1229px] top-[743px] absolute" src="https://placehold.co/30x30" />
+  <img class="w-7 h-7 left-[1299px] top-[743px] absolute" src="https://placehold.co/30x30" />
+</div>
+</body>
+</html>
